141376444c530f5ff56fe562b35e0d4cbf30fc0d
Tazonacvlishvili <nacvlishvilitazo1@gmail.com>
2022-09-22 23:26:31 +0400

    first commit

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+name =  "tazo"
+
+
+surname = "nacvlishvili"
+
+
+age = "13"
+
+
+
+height = "1.50"
+
+
+print (" me var " + name + " chemi gvaria " +  surname + " var " + age +  " wlis " + "da simagleshi var " + height)
